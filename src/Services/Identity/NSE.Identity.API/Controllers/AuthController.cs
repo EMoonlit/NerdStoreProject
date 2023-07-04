70f11a21124ea23f5ce5508db1f7d9461ff4367e
Emerson Junqueira <emoonlit@gmail.com>
2023-07-03 22:57:42 -0300

    feat: added auth controllers and base user view models

--- src/Services/Identity/NSE.Identity.API/Controllers/AuthController.cs
+++ src/Services/Identity/NSE.Identity.API/Controllers/AuthController.cs
@@ -1,6 +1,56 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identity.API.Models;
+
 namespace NSE.Identity.API.Controllers;
 
-public class AuthController
+[Route("api/identity")]
+public class AuthController : Controller
 {
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
     
+    [HttpPost("new-account")]
+    public async Task<ActionResult> Register(UserRegister userRegister)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+
+        var user = new IdentityUser
+        {
+            UserName = userRegister.Email,
+            Email = userRegister.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, userRegister.Password);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest();
+        }
+
+        await _signInManager.SignInAsync(user, false);
+        return Ok();
+    }
+
+    [HttpPost("auth")]
+    public async Task<ActionResult> Login(UserLogin userLogin)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+
+        var result = await _signInManager.PasswordSignInAsync(userLogin.Email, userLogin.Password, false, true);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
 }
--- src/Services/Identity/NSE.Identity.API/Models/UserViewModels.cs
+++ src/Services/Identity/NSE.Identity.API/Models/UserViewModels.cs
@@ -1,6 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NSE.Identity.API.Models;
 
-public class UserViewModels
+public class UserRegister
 {
+    [Required(ErrorMessage = "The field {0} is required")]
+    [EmailAddress(ErrorMessage = "Field {0} is in invalid format")]
+    public string Email { get; set; }
     
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters long", MinimumLength = 6)]
+    public string Password { get; set; }
+    
+    [Compare("Password", ErrorMessage = "Passwords don't match")]
+    public string ConfirmationPassword { get; set; }
+}
+
+public class UserLogin
+{
+    [Required(ErrorMessage = "The field {0} is required")]
+    [EmailAddress(ErrorMessage = "Field {0} is in invalid format")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters long", MinimumLength = 6)]
+    public string Password { get; set; }
 }
