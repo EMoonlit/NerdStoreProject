ac5f4f9a5e1b67dfcded5900d54085e81204877b
Emerson Junqueira <emoonlit@gmail.com>
2023-07-12 20:03:04 -0300

    feat: added base pages login and register

--- src/Web/MVC/NSE.WebApp.MVC/Controllers/AuthController.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Controllers/AuthController.cs
@@ -1,8 +1,58 @@
 using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers;
 
 public class AuthController : Controller
 {
+    [HttpGet]
+    [Route("new-account")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("new-account")]
+    public async Task<IActionResult> Register(UserRegister userRegister)
+    {
+        if (!ModelState.IsValid) return View(userRegister);
+        
+        // Send User to Identity API
+        
+        // if error return View(userRegister);
+        
+        // Login success -> return RedirectToAction("Index", "controllerName")
+        
+        return await Ok();
+    }
+
+    [HttpGet]
+    [Route("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
     
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login(UserLogin userLogin)
+    {
+        if (!ModelState.IsValid) return View(userLogin);
+        
+        // Send User to Identity API
+        
+        // if error return View(userLogin);
+        
+        // Login success -> return RedirectToAction("Index", "controllerName")
+        
+        return await Ok();
+    }
+
+    [HttpGet]
+    [Route("exit")]
+    public async Task<IActionResult> Logout()
+    {
+        // success -> return RedirectToAction("Index", "controllerName")
+    }
 }
---
+++ src/Web/MVC/NSE.WebApp.MVC/Models/UserViewModel.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NSE.WebApp.MVC.Models;
+
+public class UserRegister
+{
+    [Required(ErrorMessage = "The field {0} is required")]
+    [EmailAddress(ErrorMessage = "Field {0} is in invalid format")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters long", MinimumLength = 6)]
+    public string Password { get; set; }
+    
+    [Compare("Password", ErrorMessage = "Passwords don't match")]
+    public string ConfirmationPassword { get; set; }
+}
+
+public class UserLogin
+{
+    [Required(ErrorMessage = "The field {0} is required")]
+    [EmailAddress(ErrorMessage = "Field {0} is in invalid format")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters long", MinimumLength = 6)]
+    public string Password { get; set; }
+}
+
+public class UserLoginResponse
+{
+    public string AccessToken { get; set; }
+    public double ExpiresIn { get; set; }
+    public UserTokenData UserTokenData { get; set; }
+}
+
+public class UserTokenData
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<UserClaim> Claims { get; set; }
+}
+
+public class UserClaim
+{
+    public string Value { get; set; }
+    public string Type { get; set; }
+}
