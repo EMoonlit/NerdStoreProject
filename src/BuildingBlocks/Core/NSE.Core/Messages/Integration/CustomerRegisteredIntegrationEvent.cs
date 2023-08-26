0bb41d3353b6dc0ec4c262164247ab15ba0bb71f
Emerson Junqueira <emoonlit@gmail.com>
2023-08-26 14:37:00 -0300

    feat: added core event and message response

---
+++ src/BuildingBlocks/Core/NSE.Core/Messages/Integration/CustomerRegisteredIntegrationEvent.cs
@@ -0,0 +1,17 @@
+namespace NSE.Core.Messages.Integration;
+
+public class CustomerRegisteredIntegrationEvent : IntegrationEvent
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Cpf { get; private set; }
+
+    public CustomerRegisteredIntegrationEvent(Guid id, string name, string email, string cpf)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Cpf = cpf;
+    }
+}
